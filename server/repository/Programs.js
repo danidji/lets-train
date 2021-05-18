2d7ba3f98130d7e0572a9debdbda8d5a31146a61
dany gbelidji <dany.gbelidji@live.fr>
2021-05-18 12:23:28 +0200

    🗃️ ADD - repository Programs

---
+++ server/repository/Programs.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// connexion à la base de donnée 
+require('./server/bdd/database')();
+
+// Schéma de donées propres aux programmes 
+
+const programSchema = mongoose.Schema({
+    name: { type: String }
+    , level: { type: Number }
+    , description: { type: String }
+    , poster_image: { type: String }
+
+}, { versionKey: false });
+
+module.exports = class Programs {
+    constructor() {
+        this.db = mongoose.model('program', programSchema);
+    }
+}
