31174d394fb40a39eaf2c94e6a74f2b6ad34a0c2
dany gbelidji <dany.gbelidji@live.fr>
2021-05-21 11:05:50 +0200

    WIP - display List program on page admin

--- client/src/components/ProgramsList.jsx
+++ client/src/components/ProgramsList.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-
+import axios from "axios";
+import { response } from 'express';
 
 export default function ProgramsList(props) {
-    useEffect(() => {
+    // useEffect(() => {
+    //     let data;
+    //     axios.get('/api/programmes/liste').then((response) => {
+    //         console.log(`axios.get -> response`, response.data)
+    //         data = response.data;
+    //     })
+    // }, [])
+    return (
+        <>
+            <h1>YOoooo</h1>
+            <div className="data">{ }</div>
 
-    })
+        </>
+    )
+
 
 
 }
